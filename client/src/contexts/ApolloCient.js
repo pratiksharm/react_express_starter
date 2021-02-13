35172fe0afd0b01780d8159e6800445a12c699c7
pratiksharm <sharma.pratik2016@gmail.com>
2021-02-13 13:01:24 +0530

    this should work

--- client/src/contexts/ApolloCient.js
+++ client/src/contexts/ApolloCient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'http://localhost:5000/graphql' || "https://journaling-iterate.herokuapp.com/graphql",
   cache: new InMemoryCache()
 });
 
--- server.js
+++ server.js
@@ -15,8 +15,21 @@ const server = new ApolloServer({
 
 const app = express();
 app.use(express.json())
-app.use(cors())
 
+const whitelist = ['http://localhost:3000'​, 'http://localhost:8080'​, 'https://journaling-iterate.herokuapp.com'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
